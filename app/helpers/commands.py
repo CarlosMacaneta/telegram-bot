4710c4d08b456664168a7244acd5918f936ff0e6
Carlos Macaneta <56470814+CarlosMacaneta@users.noreply.github.com>
2024-07-22 15:19:44 +0200

    bot version: 1.0 (#7)
    * [TBOT-01]: feat: implementing telegram message handler  (#1)
    
    * feat: implementing message handler
    
    * chore: removing unused imports
    
    * package setup (#2)
    
    * fix: fixing commands on reply (#3)
    
    * fix: fixing imports (#4)
    
    * bot initialization (#5)
    
    * feat: adds example dotenv (#6)

--- app/helpers/commands.py
+++ app/helpers/commands.py
@@ -1,13 +1,13 @@
 from telegram import Update
-from telegram import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import ContextTypes
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.reply_text('Hello! Thanks for starting a new conversation. I can teach you everything!')
+    await update.message.reply_text('Hello! Thanks for starting a new conversation. I can teach you everything!')
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.reply_text('I am nerd! Please type something to help.')
+    await update.message.reply_text('I am nerd! Please type something to help.')
 
 
 async def custom_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.reply_text('This is a custom command.')
+    await update.message.reply_text('This is a custom command.')
--- main.py
+++ main.py
@@ -1,6 +1,25 @@
 import os
-from telegram import Update
-from telegram import Application, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from app.helpers.commands import start_command, help_command, custom_command
+from app.helpers.message_handler import errors, handle_message
 
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.reply_text('Hello! Thanks for starting a new conversation. I can teach you everything!')
+
+if __name__ == "__main__":
+    print("Starting bot...")
+    app = Application.builder().token(os.environ.get('TOKEN_KEY')).build()
+    
+    # Command handlers
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('help', help_command))
+    app.add_handler(CommandHandler('custom', custom_command))
+    
+    # Message handlers
+    app.add_handler(MessageHandler(filters.TEXT, handle_message))
+    
+    # Error handlers
+    app.add_error_handler(errors)
+    
+    
+    # checking app updates
+    print("Polling...")
+    app.run_polling(poll_interval=5)
