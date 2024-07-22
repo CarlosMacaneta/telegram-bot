8faf5b7151ea262cc3a01efa79d5f270b5c603cb
Carlos Macaneta <carlosmacaneta@hotmail.com>
2024-07-22 18:52:02 +0200

    delete: removing gemini_ai.py file

--- gemini_ai.py
+++
@@ -1,9 +0,0 @@
-import google.generativeai as genai
-import os
-
-genai.configure(api_key=os.environ.get('GEMINI_TOKEN_KEY'))
-
-model = genai.GenerativeModel(os.environ.get('GEMINI_MODEL_NAME'))
-
-response = model.generate_content("Write a story about an AI and magic")
-print(response.text)
